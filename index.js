3edd6ccdc16c1f7696fffd667eedc58fb60d955c
fredtyfirst <114894370+fredtyfirst@users.noreply.github.com>
2024-02-04 13:53:04 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,81 @@
+var tab1 = document.getElementById("tab1");
+var tab2 = document.getElementById("tab2");
+var tab3 = document.getElementById("tab3");
+var tab4 = document.getElementById("tab4");
+
+
+
+var content1 = document.getElementById("content1");
+var content2 = document.getElementById("content2");
+var content3 = document.getElementById("content3");
+var content4 = document.getElementById("content4");
+
+
+
+tab1.classList.add('active');
+content1.classList.add('active');
+
+
+
+tab1.onclick = function() {
+  tab1.classList.add('active');
+  content1.classList.add('active');
+
+  tab2.classList.remove('active');
+  content2.classList.remove('active');
+
+  tab3.classList.remove('active');
+  content3.classList.remove('active');
+
+  tab4.classList.remove('active');
+  content4.classList.remove('active');
+
+}
+
+tab2.onclick = function() {
+  tab2.classList.add('active');
+  content2.classList.add('active');
+
+  tab1.classList.remove('active');
+  content1.classList.remove('active');
+
+  tab3.classList.remove('active');
+  content3.classList.remove('active');
+
+  tab4.classList.remove('active');
+  content4.classList.remove('active');
+
+}
+
+
+tab3.onclick = function() {
+  tab3.classList.add('active');
+  content3.classList.add('active');
+
+  tab1.classList.remove('active');
+  content1.classList.remove('active');
+
+  tab2.classList.remove('active');
+  content2.classList.remove('active');
+
+  tab4.classList.remove('active');
+  content4.classList.remove('active');
+
+}
+
+
+tab4.onclick = function() {
+  tab4.classList.add('active');
+  content4.classList.add('active');
+
+  tab1.classList.remove('active');
+  content1.classList.remove('active');
+
+  tab3.classList.remove('active');
+  content3.classList.remove('active');
+
+  tab2.classList.remove('active');
+  content2.classList.remove('active');
+
+}
+
